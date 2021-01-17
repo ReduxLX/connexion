eb15d9c431b4ad049df12aa90e85b06e46540c60
Alfons Fernaldy <alfonsfernaldy42@gmail.com>
2021-01-17 21:37:57 +0700

    Updated assets.

--- connexion-app/src/screens/About/About.js
+++ connexion-app/src/screens/About/About.js
@@ -2,11 +2,11 @@ import React from "react";
 import styled from "styled-components";
 import Avatar from "@material-ui/core/Avatar";
 import { styled as muiStyled } from "@material-ui/styles";
-import TeamImg from "../../res/images/team.jpg";
+import TeamImg from "../../res/images/team2.jpg";
 import { PageWrapper } from "../SharedStyles";
 import Theme from "../../Theme";
 
-const AboutUsText = `Hello dear viewer, we are a brother-sister team studying at Monash University Malaysia. On the right is myself, Alfons Fernaldy, and on the left is my sister, Michelle Adeline. This project represents our final submission for the Monash Quickhack Hackathon 2021 which was about "Connecting the past present and future". We thought of many solutions to this problem but came to a realization that Monash lacked a truly unified platform where current, past and future students could discuss matters freely. Thus this was how Connexion was born.`;
+const AboutUsText = `Hello dear viewer, we are a brother-sister team studying at Monash University Malaysia. On the left is myself, Alfons Fernaldy, and on the right is my sister, Michelle Adeline. This project represents our final submission for the Monash Quickhack Hackathon 2021 which was about "Connecting the past present and future". We thought of many solutions to this problem but came to a realization that Monash lacked a truly unified platform where current, past and future students could discuss matters freely. Thus this was how Connexion was born.`;
 
 const HowItWorksText = `In order to connect the past, present and future, our platform needed to cater for the following subcateogries: General, Future Monashians,
                         Freshmen, Seniors and After Monash. When users create a post, they must choose between 1 to 3 categories which correlate to their topic.
@@ -66,7 +66,7 @@ const About = () => {
 
 const AboutSection = styled.div`
   display: flex;
-  margin-top: 2rem;
+  margin-top: 3rem;
   align-items: center;
   text-align: right;
   & > * {
@@ -125,13 +125,11 @@ const TeamImage = muiStyled(Avatar)({
   width: "350px",
   height: "250px",
   marginRight: "5rem",
-  marginTop: "6rem",
-  border: "solid",
-  borderColor: Theme.colors.disabled,
-  borderWidth: "8px",
+  marginTop: "1rem",
   "@media(max-width:768px)": {
     width: "250px",
     height: "150px",
+    marginRight: "0rem",
   },
 });
 
